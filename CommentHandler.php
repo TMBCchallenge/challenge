775f12598bc1f48f38717426a30e38756a61cac4
Bruce Hellstrom <bruce@celebritycc.com>
2022-04-06 20:52:13 -0700

    TMBC Challenge
    - Finished PHP portion first using the data in the CommentHandler.php
      file only. It does not use my refactored db portion of the test.
    - Finshed db rewrite to make it more efficient and be able to return
      all comments in the proper order using the SQL query only.
    - Finished the webform portion as best as I could.  There were some
      assumptions made.

--- CommentHandler.php
+++ CommentHandler.php
@@ -26,8 +26,60 @@
  * -comment
  * -create_date
  *
+ * Note from BH *****************************************************
+ * This was written first before my refactor of the database and method of access
+ * so it is based on the information given above only and I'm assuming a data
+ * set that is not ordered with only the parent id as a guide to the location
+ * of the items in the list.
+ * *******************************************************************
  */
-Class CommentHandler {
+
+interface CommentHandling {
+    public function getComments();
+
+    public function addComment($comment);
+}
+
+
+Class CommentHandler implements CommentHandling {
+
+    /**
+     * mySqli - A mysqli instance set into our class
+     *          so that unit/integratin testing can be done
+     *          more easily by injecting mocks or custom
+     *          database connections
+     */
+    private mysqli $mySqli = NULL;
+
+    public constant MAX_DEPTH = 2;
+
+    /**
+     * Constructor
+     *
+     * @param: mySqli - A mysqli object to use for database access
+     */
+    public function __construct(mysqli $mySqli) {
+        $this->mySqli = $mySqli;
+    }
+
+    /**
+     * setMysqli - Sets the internal Mysqli object
+     *
+     * @param: mySqli - A mysqli object to use for database access
+     */
+    public function setMysqli(mysqli $mySqli) : void {
+        $this->mySqli = $mySqli;
+    }
+
+    /**
+     * getMysqli - Gets the internal Mysqli object
+     *
+     * @return: internal mysqli object or NULL
+     */
+    public function getMysqli() : ?mysqli {
+        return($this->mySlqi);
+    }
+
     /**
      * getComments
      *
@@ -35,31 +87,19 @@ Class CommentHandler {
      *
      * @return array
      */
-    public function getComments() {
-        $db = new mysql('testserver', 'testuser', 'testpassword');
-        $sql = "SELECT * FROM comments_table where parent_id=0 ORDER BY create_date DESC;";
-        $result = mysql_query($sql, $db);
-        $comments = [];
-        while ($row = mysql_fetch_assoc($result)) {
-            $comment = $row;
-            $reply_1_sql = "SELECT * FROM comments_table where parent_id=" . $row['id'] . " ORDER BY create_date DESC;";
-            $result_reply_1 = mysql_query($reply_1_sql, $db);
-            $replies = [];
-            while ($row1 = mysql_fetch_assoc($result)) {
-                $reply = $row1;
-                $reply_2_sql = "SELECT * FROM comments_table where parent_id=" . $row1['id'] . " ORDER BY create_date DESC;";
-                $result_reply_2 = mysql_query($reply_2_sql, $db);
-                $replies_to_replies = [];
-                while ($row2 = mysql_fetch_assoc($result)) {
-                    $replies_to_replies[] = $row2;
-                }
-                $reply['replies'] = $replies_to_replies;
-                $replies[] = $reply;
+    public function getComments() : mixed {
+        $comments_return = [];
+        $cur_depth = 0;
+
+        try {
+            // Get the root level comments
+            $comments_return = queryComments(0);
+                $comments_return[] = $comment;
+            } catch(Exception $ex) {
+                throw $ex;
             }
-            $comment['replies'] = $replies;
-            $comments[] = $comment;
         }
-        return $comments;
+       return $comments_return;
     }
 
     /**
@@ -68,20 +108,102 @@ Class CommentHandler {
      * This function accepts the data directly from the user input of the comment form and creates the comment entry in the database.
      *
      * @param $comment
-     * @return string or array
+     * @return: Number of rows affected
      */
-    public function addComment($comment) {
-        $db = new mysql('testserver', 'testuser', 'testpassword');
-        $sql = "INSERT INTO comments_table (parent_id, name, comment, create_date) VALUES (" . $comment['parent_id'] . ", " . $comment['name'] . ", " . $comment['comment'] . ", NOW())";
-        $result = mysql_query($sql, $db);
-        if($result) {
-            $id = mysql_insert_id();
-            $sql = "SELECT * FROM comments_table where id=" . $id . ";";
-            $result = mysql_query($sql, $db);
-            $comment = mysql_result($result, 0);
-            return $comment;
-        } else {
-            return 'save failed';
+
+    public function addComment(mixed $comment) : int {
+        int $retval = -1;
+
+        // Using a prepared statment handles possible SQL injection attacks since we are getting the data
+        // directly from user input.  Assuming create date is not getting passed in from the web form and
+        // we are using mysql DATETIME for the field in the db.
+        $prep_statement = "INSERT into comments_table (parent_id, `name`, comment, create_date) VALUES(?, ?, ?, NOW())";
+
+        $db = $this->getMysqli();
+
+        if ($db === NULL) {
+            throw new Exception("CommentHandler::addComment(): NULL Mysqli object");
         }
+
+        $stmt = $db->prepare($prep_statement);
+
+        if ($stmt === FALSE) {
+            throw new Exception("CommentHandler::addComment(): " . $db->error)
+        }
+        $stmt->bind_param("iss", $parent_id, $name, $comment);
+        $parent_id = $comment['parent_id'];
+        $name = $comment['name'];
+        $comment = $comment['comment'];
+
+        if ($stmt->execute() === FALSE) {
+            throw new Exception("CommentHandler::addComment(): Error executing SQL INSERT: " . $db->error);
+        }
+
+        $retval = $stmt->affected_rows;
+        return $retval;
     }
+
+    /**
+     * convertMysqlDateToPHP - Converts a Mysql DATETIME value into a more readable PHP string
+     *
+     * @param $datetime - A Mysql DATETIME formatted string
+     *
+     * @return string representing time in mm/dd/yyyy hh:mm:ss format.
+     *
+     */
+    protected function convertMysqlDateToPHP(string $datetime) : string {
+        $phptime = strtotime($datetime);
+        return(date("m/d/y H:i", $phptime));
+    }
+
+    /**
+     * queryComments - function designed to be called recursively to obtain comment records in
+     *                 the proper sequence
+     *
+     * @param $parent_id - the current parent_id to get the records for
+     *
+     * @param $current_depth - the current depth of recursion currently limited to 2 levels deep
+     *
+     * @return an array of comment records sorted correctly
+     */
+    protected function queryComments(int $parent_id, int $cur_depth) : mixed {
+        $comments_return = [];
+
+        $prep_statement = "SELECT id, parent_id, `name`, comment, create_date "
+            . "FROM comments_table WHERE parent_id = ? ORDER BY create_date DESC";
+        $db = $this->getMysqli();
+
+        if ($db === NULL) {
+            throw new Exception("CommentHandler::queryComments(): NULL Mysqli object");
+        }
+
+        $stmt = $db->prepare($prep_statement);
+
+        if ($stmt === FALSE) {
+            throw new Exception("CommentHandler::queryComments(): " . $db->error)
+        }
+        $stmt->bind_param("i", $parent_id);
+
+        $stmt->execute();
+        $query_result = $stmt->get_result();
+
+        while($comment_row = $query_result->fetch_assoc()) {
+            // convert mysql datetime to a PHP formatted date/time.  Will be most likely
+            // stored in server time zone.  More could be done to nornmalize the date.
+            $comment_row['create_date'] = convertMysqlDateToPHP($comment_row['create_date']);
+            $comments_return[] = $comment_row;
+
+            if ($cur_depth < MAX_DEPTH) {
+                $child_comments = queryComments($comment_row['id'], $cur_depth + 1);
+                if (count($child_comments) > 0) {
+                    $comments_return = array_merge($comments_return, $child_comments);
+                }
+            }
+        }
+        $query_result->close();
+        $stmt->close();
+
+        return $comments_return;
+    }
+
 }
--- db.sql
+++ db.sql
@@ -12,16 +12,52 @@ Consider adding foreign key constraints, indices etc.
 
 /* AUTHOR TABLE */
 CREATE TABLE `author` (
-  `id`,
-  `comment` varchar(2000)
-) ENGINE=InnoDB AUTO_INCREMENT=2046711 DEFAULT CHARSET=utf8 ROW_FORMAT=DYNAMIC;
+  `id` BIGINT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+  `first_name` VARCHAR(50)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8 ROW_FORMAT=DYNAMIC;
 
 /* COMMENT TABLE */
 CREATE TABLE `comment` (
-  `id`,
-  `first_name` varchar(20)
-) ENGINE=InnoDB AUTO_INCREMENT=2046711 DEFAULT CHARSET=utf8 ROW_FORMAT=DYNAMIC;
+  `id` BIGINT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+  `parent_id` BIGINT UNSIGNED NOT NULL, /* Not really necessary anymore with this implementation */
+  `author_id` BIGINT UNSIGNED NOT NULL,
+  `comment` VARCHAR(2000),
+  `created_date` DATETIME DEFAULT '1970-01-01 00:00:00',
+  `is_root` BOOLEAN DEFAULT false NOT NULL,
+  FOREIGN KEY (`author_id`) REFERENCES author(`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8 ROW_FORMAT=DYNAMIC;
+
+/* COMMENT_CLOSURE TABLE */
+CREATE TABLE `comment_closure` (
+    `ancestor` BIGINT UNSIGNED NOT NULL,
+    `descendant` BIGINT UNSIGNED NOT NULL,
+    `path` VARCHAR(10000) NOT NULL,
+    FOREIGN KEY (`ancestor`) REFERENCES `comment`(`id`),
+    FOREIGN KEY (`descendant`) REFERENCES `comment`(`id`),
+    KEY `ind_anc_des` (`ancestor`, `descendant`) USING BTREE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 ROW_FORMAT=DYNAMIC;
 
 /* QUERY */
-SELECT * FROM comments;
+WITH comments_virtual AS
+    (SELECT com.id, com.parent_id, a.first_name as `name`, com.comment, com.created_date, cl.path
+    FROM comment com
+        INNER JOIN author a
+            ON a.id = com.author_id
+        INNER JOIN comment_closure cl
+            ON com.id = cl.descendant
+    WHERE com.is_root IS TRUE
+        AND cl.ancestor = cl.descendant
+    UNION
+    SELECT com.id, com.parent_id a.first_name as `name`, com.comment, com.created_date, cl.path
+    FROM comment com
+        INNER JOIN author a
+            ON a.id = com.author_id
+        INNER JOIN comment_closure cl
+            ON com.id = cl.descendant
+    WHERE cl.ancestor = (SELECT id FROM comment WHERE is_root IS TRUE)
+        AND cl.ancestor != cl.descendant)
+
+SELECT id, parent_id, `name`, comment, created_date
+FROM comments_virtual
+ORDER BY path;
 
